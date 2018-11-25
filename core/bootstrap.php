addc81cc15de0276afded9d6b145195e1ea48886
Mahdi Abedi <mahdiabedi220@yahoo.com>
2018-11-25 23:08:05 +0330

    یکشنبه4 آذر
    تغییرات یکشنبه 4 آدر 97

---
+++ core/bootstrap.php
@@ -0,0 +1,17 @@
+<?php 
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+
+//Class Autoloader
+function __autoload($className){
+    if(file_exists(ROOT . DS . 'core' . DS . $className . '.php')){
+        require_once(ROOT . DS . 'core' . DS . $className . '.php');
+    }elseif(file_exists(ROOT . DS . 'app' . DS .'controllers' . DS . $className . '.php')){
+        require_once(ROOT . DS . 'app' . DS .'controllers' . DS . $className . '.php');
+    }elseif(file_exists(ROOT . DS . 'app' . DS .'models' . DS . $className . '.php')){
+        require_once(ROOT . DS . 'app' . DS .'models' . DS . $className . '.php');
+    }
+}
+
+//Route The Request
+Router::route($url);
